40080b3bd8a8bc90d993ebdfe3d9254dbb340788
Ognyan-Petranov <58415140+Ognyan-Petranov@users.noreply.github.com>
2020-08-16 22:00:13 +0300

    Add files via upload

---
+++ FastFood.Models/Order.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using FastFood.Models.Enums;
+
+namespace FastFood.Models
+{
+
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Customer { get; set; }
+
+        [Required]
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(OrderType))]
+        public OrderType Type { get; set; }
+
+        [NotMapped]
+        public decimal TotalPrice { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        [Required]
+        public Employee Employee { get; set; }
+
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
